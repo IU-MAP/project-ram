a331cf48c3527b11c3d27d536068d8b25264a8a2
Alfiya <alphiya01@mail.ru>
2021-04-26 23:46:06 +0300

    Added generation of insert file with random values close *18*

---
+++ src/data/unify.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script is intend to unify lines in the files in current directory
+# @unaimillan
+
+for fl in ./*.txt;
+do
+	echo "$fl: "
+	wc -l $fl
+	cat $fl | sort -u | wc -l
+	cat $fl | sort -u | tee $fl 1> /dev/null
+done
